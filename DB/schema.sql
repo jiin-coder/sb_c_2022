fd1f91d85f84ceab5abd43f826f9736dfb136e4b
jiin-coder <fpahstiq123@naver.com>
2022-02-10 13:46:44 +0900

    db/schema.sql 파일 추가

---
+++ DB/schema.sql
@@ -0,0 +1,34 @@
+# DB 생성
+DROP DATABASE IF EXISTS sb_c_2022;
+CREATE DATABASE sb_c_2022;
+USE sb_c_2022;
+
+# 게시물 테이블 생성
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+# 게시물 테스트 데이터 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3';
+
+SELECT * FROM article;
