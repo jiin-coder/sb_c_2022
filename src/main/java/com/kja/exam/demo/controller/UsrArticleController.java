56a7138e5fc8041203ec4a88d8f5c3dec3455440
jiin-coder <fpahstiq123@naver.com>
2022-02-10 10:13:55 +0900

    테스트용 게시물 10개 자동생성

--- src/main/java/com/kja/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kja/exam/demo/controller/UsrArticleController.java
@@ -17,9 +17,24 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		articlesLastId = 0;
 		articles = new ArrayList<>();
+		
+		makeTestData();
 	}
 	
-
+	private void makeTestData() {
+		for ( int i = 1; i <= 10; i++ ) {
+			int id = articlesLastId + 1;
+			String title = "제목 " + i;
+			String body = "내용 " + i;
+			
+			Article article = new Article(id, title, body);
+			
+			articles.add(article);
+			articlesLastId = id;
+		}
+	}
+	
+	
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -28,6 +43,7 @@ public class UsrArticleController {
 		
 		articles.add(article);
 		articlesLastId = id;
+		
 		return article;
 	}
 	// http://localhost:8011/usr/article/doAdd?title=제목1&body=1
