518c498abc8067fa74682093ba28d59a7dedaf99
jiin-coder <fpahstiq123@naver.com>
2022-02-14 13:22:43 +0900

    회원가입 시, 입력데이터의 유효성체크를 Ut.empty 함수로 수정

---
+++ src/main/java/com/kja/exam/demo/util/Ut.java
@@ -0,0 +1,17 @@
+package com.kja.exam.demo.util;
+
+public class Ut {
+	public static boolean empty(Object obj) {
+		if ( obj == null ) {
+			return true;
+		}
+		
+		if (obj instanceof String == false) {
+			return true;
+		}
+		
+		String str = (String) obj;
+		
+		return str.trim().length() == 0;
+	}
+}
