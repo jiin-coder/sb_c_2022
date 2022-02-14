9df4c59266fce05cd69c7dcc2ab30503c7ab852c
jiin-coder <fpahstiq123@naver.com>
2022-02-14 13:32:05 +0900

    회원가입 시, 이름+이메일 중복체크

--- src/main/java/com/kja/exam/demo/service/MemberService.java
+++ src/main/java/com/kja/exam/demo/service/MemberService.java
@@ -13,20 +13,31 @@ public class MemberService {
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
-	
+
 	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		// 기존에 로그인id 중복체크
 		Member oldmember = getMemberByLoginId(loginId);
-		
-		if ( oldmember != null ) {
+
+		if (oldmember != null) {
 			return -1;
 		}
-		
+
+		// 이름+이메일 중복체크
+		oldmember = getMemberByNameAndEmail(name, email);
+
+		if (oldmember != null) {
+			return -2;
+		}
+
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		return memberRepository.getLastInsertId();
 	}
 
+	private Member getMemberByNameAndEmail(String name, String email) {
+		return memberRepository.getMemberByNameAndEmail(name, email);
+	}
+
 	private Member getMemberByLoginId(String loginId) {
-		// TODO Auto-generated method stub
 		return memberRepository.getMemberByLoginId(loginId);
 	}
 
