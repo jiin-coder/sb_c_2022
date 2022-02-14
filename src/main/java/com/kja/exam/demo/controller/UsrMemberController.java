e468759f4dc3d0c1f0a0b3f85856fafd6e5d6f28
jiin-coder <fpahstiq123@naver.com>
2022-02-14 12:50:11 +0900

    회원가입 시, 입력데이터 유효성 체크

--- src/main/java/com/kja/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/kja/exam/demo/controller/UsrMemberController.java
@@ -19,6 +19,31 @@ public class UsrMemberController {
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
+		
+		if ( loginId == null ) {
+			return "loginId(을)를 입력해주세요.";
+		}
+		
+		if ( loginPw == null ) {
+			return "loginPw(을)를 입력해주세요.";
+		}
+		
+		if ( name == null ) {
+			return "name(을)를 입력해주세요.";
+		}
+		
+		if ( nickname == null ) {
+			return "nickname(을)를 입력해주세요.";
+		}
+		
+		if ( cellphoneNo == null ) {
+			return "cellphoneNo(을)를 입력해주세요.";
+		}
+		
+		if ( email == null ) {
+			return "email(을)를 입력해주세요.";
+		}
+		
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		
 		if ( id == -1 ) {
