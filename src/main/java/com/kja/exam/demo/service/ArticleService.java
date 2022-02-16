688e35f11178a3c840ceba357fba136c9c7e0dc1
jiin-coder <fpahstiq123@naver.com>
2022-02-16 10:13:34 +0900

    /usr/article/doAdd에 ResultData 적용

--- src/main/java/com/kja/exam/demo/service/ArticleService.java
+++ src/main/java/com/kja/exam/demo/service/ArticleService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.kja.exam.demo.repository.ArticleRepository;
+import com.kja.exam.demo.util.Ut;
 import com.kja.exam.demo.vo.Article;
+import com.kja.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
@@ -15,9 +17,11 @@ public class ArticleService {
 		this.articleRepository = articleRepository;
 	}
 
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
-		return articleRepository.getLastInsertId(); 
+		int id = articleRepository.getLastInsertId();
+		
+		return ResultData.from("S-1",  Ut.f("%d번 게시물이 생성되었습니다.",id), id); 
 	}
 
 	public List<Article> getArticles() {
