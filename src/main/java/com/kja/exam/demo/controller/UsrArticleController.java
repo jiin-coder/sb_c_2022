190dfd49d217e38b5944e6ac1ae095c20e8eb6ef
jiin-coder <fpahstiq123@naver.com>
2022-02-16 11:09:04 +0900

    /usr/article/doAdd에 ResultData.newData 적용

--- src/main/java/com/kja/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kja/exam/demo/controller/UsrArticleController.java
@@ -39,7 +39,7 @@ public class UsrArticleController {
 		
 		Article article = articleService.getArticle(id);
 
-		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
+		return ResultData.newData(writeArticleRd, article);
 	}
 	// http://localhost:8011/usr/article/doAdd?title=제목1&body=1
 
