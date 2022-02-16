fdab7bd9d309c36f9294c581c0d22313c81db865
jiin-coder <fpahstiq123@naver.com>
2022-02-16 11:16:24 +0900

    /usr/article/doDelete, doModify에에 ResultData 적용

--- src/main/java/com/kja/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kja/exam/demo/controller/UsrArticleController.java
@@ -11,48 +11,43 @@ import com.kja.exam.demo.util.Ut;
 import com.kja.exam.demo.vo.Article;
 import com.kja.exam.demo.vo.ResultData;
 
-
 @Controller
 public class UsrArticleController {
 
 	private ArticleService articleService;
-	
+
 	public UsrArticleController(ArticleService articleService) {
 		this.articleService = articleService;
 	}
 
-	
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public ResultData doAdd(String title, String body) {
-		if ( Ut.empty(title) ) {
+	public ResultData<Article> doAdd(String title, String body) {
+		if (Ut.empty(title)) {
 			return ResultData.from("F-1", "title(을)를 입력해주세요.");
 		}
-		
-		if ( Ut.empty(body) ) {
+
+		if (Ut.empty(body)) {
 			return ResultData.from("F-2", "body(을)를 입력해주세요.");
 		}
-		
-		
-		ResultData<Integer> writeArticleRd  = articleService.writeArticle(title, body);
+
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body);
 		int id = writeArticleRd.getData1();
-		
+
 		Article article = articleService.getArticle(id);
 
 		return ResultData.newData(writeArticleRd, article);
 	}
 	// http://localhost:8011/usr/article/doAdd?title=제목1&body=1
 
-
-	@RequestMapping("/usr/article/getArticles") 
+	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
-	public ResultData getArticles() {
+	public ResultData<List<Article>> getArticles() {
 		List<Article> articles = articleService.getArticles();
-		
-		return ResultData.from("S-1","게시물 리스트입니다.", articles);
+
+		return ResultData.from("S-1", "게시물 리스트입니다.", articles);
 	}
 	// http://localhost:8011/usr/article/getArticles
-	
 
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
@@ -67,35 +62,33 @@ public class UsrArticleController {
 	}
 	// http://localhost:8011/usr/article/getArticle?id=6
 
-
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public String doDelete(int id) {
+	public ResultData<Integer> doDelete(int id) {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 게시물이 존재하지 않습니다.";
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 
 		articleService.deleteArticle(id);
 
-		return id + "번 게시물을 삭제하였습니다.";
+		return ResultData.from("S-1", Ut.f("%d번 게시물을 삭제하였습니다.", id), id);
 	}
 	// http://localhost:8011/usr/article/doDelete?id=1
 
-
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public String doModify(int id, String title, String body) {
+	public ResultData<Integer> doModify(int id, String title, String body) {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 게시물이 존재하지 않습니다.";
+			ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 
 		articleService.modifyArticle(id, title, body);
 
-		return id + "번 게시물을 수정하였습니다.";
+		return ResultData.from("S-1", Ut.f("%d번 게시물을 수정하였습니다.", id), id);
 	}
 	// http://localhost:8011/usr/article/doModify?id=1&title=asdasdas&body=asdasd
 }
