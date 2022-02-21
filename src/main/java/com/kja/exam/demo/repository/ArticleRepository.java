b00cba886f076fd4215ce1d000e4d7c0f19c4f03
jiin-coder <fpahstiq123@naver.com>
2022-02-21 11:46:45 +0900

    게시물 리스트에서 작성자명 표기

--- src/main/java/com/kja/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/kja/exam/demo/repository/ArticleRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import com.kja.exam.demo.vo.Article;
 
@@ -17,6 +18,14 @@ public interface ArticleRepository {
 
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 
+	@Select("""
+			SELECT A.*,
+			M.nickname AS extra__writerName
+			FROM article AS A
+			LEFT JOIN member AS M
+			ON A.memberId = M.id
+			ORDER BY A.id DESC
+			""")
 	public List<Article> getArticles();
 
 	public int getLastInsertId();
--- src/main/webapp/WEB-INF/jsp/usr/article/list.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/article/list.jsp
@@ -31,8 +31,10 @@
               <td>${article.id}</td>
               <td>${article.regDate.substring(2, 16)}</td>
               <td>${article.updateDate.substring(2, 16)}</td>
-              <td>${article.memberId}</td>
-              <td><a href="../article/detail?id=${article.id}">${article.title}</a></td>
+              <td>${article.extra__writerName}</td>
+              <td>
+              <a class="hover:underline" href="../article/detail?id=${article.id}">${article.title}</a>
+              </td>
             </tr>
           </c:forEach>
         </tbody>
