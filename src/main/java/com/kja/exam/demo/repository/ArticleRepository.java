86472c87f593f0f57349e9554b291dd596208df5
jiin-coder <fpahstiq123@naver.com>
2022-02-21 12:09:05 +0900

    게시물 상세페이지에서 작성자명 표시

--- src/main/java/com/kja/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/kja/exam/demo/repository/ArticleRepository.java
@@ -12,7 +12,16 @@ import com.kja.exam.demo.vo.Article;
 public interface ArticleRepository {
 	public void writeArticle(@Param("memberId") int memberId, @Param("title") String title, @Param("body") String body);
 
-	public Article getArticle(@Param("id") int id);
+	@Select("""
+			SELECT A.*,
+			M.nickname AS extra__writerName
+			FROM article AS A
+			LEFT JOIN member AS M
+			ON A.memberId = M.id
+			WHERE 1
+			AND A.id = #{id}
+				""")
+	public Article getForPrintArticle(@Param("id") int id);
 
 	public void deleteArticle(@Param("id") int id);
 
@@ -26,7 +35,7 @@ public interface ArticleRepository {
 			ON A.memberId = M.id
 			ORDER BY A.id DESC
 			""")
-	public List<Article> getArticles();
+	public List<Article> getForPrintArticles();
 
 	public int getLastInsertId();
 }
