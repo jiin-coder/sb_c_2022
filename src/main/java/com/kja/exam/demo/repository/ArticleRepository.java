3de5c5333f1009102cfbc3577b83e2f64a83ca20
jiin-coder <fpahstiq123@naver.com>
2022-03-07 11:39:08 +0900

    게시물리스트에 선택된 게시판에 관련된 게시물만 표시

--- src/main/java/com/kja/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/kja/exam/demo/repository/ArticleRepository.java
@@ -28,14 +28,20 @@ public interface ArticleRepository {
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 
 	@Select("""
+			<script>
 			SELECT A.*,
 			M.nickname AS extra__writerName
 			FROM article AS A
 			LEFT JOIN member AS M
 			ON A.memberId = M.id
+			WHERE 1
+			<if test="boardId != 0">
+				AND A.boardId = #{boardId}
+			</if>
 			ORDER BY A.id DESC
+			</script>
 			""")
-	public List<Article> getForPrintArticles();
+	public List<Article> getForPrintArticles(@Param("boardId") int boardId);
 
 	public int getLastInsertId();
 }
