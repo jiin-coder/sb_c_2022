f575684b89063d48bd80bae78dcaeab53523853b
jiin-coder <fpahstiq123@naver.com>
2022-03-07 12:05:44 +0900

    게시물 페이징을 위한 게시물 대량생성

--- DB/schema.sql
+++ DB/schema.sql
@@ -137,3 +137,13 @@ SELECT * FROM article;
 
 SELECT * FROM board WHERE id = 1;
 SELECT * FROM board WHERE id = 2;
+
+# 게시물 대량생성
+INSERT INTO article
+(
+    regDate, updateDate, memberId, boardId, title, `body`
+)
+SELECT NOW(), NOW(), FLOOR(RAND() * 2) + 1, FLOOR(RAND() * 2) + 1, CONCAT('제목_', RAND()), CONCAT('내용_', RAND())
+FROM article;
+
+SELECT COUNT(*) FROM article;
