724e04345cd3a86d5f3c9e7a4e48777ff18e1c94
jiin-coder <fpahstiq123@naver.com>
2022-03-09 13:46:02 +0900

    리스트에서 검색타입과 검색어에 따라, 총 게시물 수 계산

--- src/main/java/com/kja/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kja/exam/demo/controller/UsrArticleController.java
@@ -33,14 +33,14 @@ public class UsrArticleController {
 	}
 	
 	@RequestMapping("/usr/article/list")
-	public String showList(Model model, @RequestParam(defaultValue = "1") int boardId, @RequestParam(defaultValue = "1") int page) {
+	public String showList(Model model, @RequestParam(defaultValue = "1") int boardId, @RequestParam(defaultValue = "title,Body") String searchKeywordTypeCode, @RequestParam(defaultValue = "") String searchKeyword, @RequestParam(defaultValue = "1") int page) {
 		Board board = boardService.getBoardById(boardId);
 		
 		if( board == null ) {
 			return rq.historyBackJsOnView(Ut.f("%d번 게시판은 존재하지 않습니다.", boardId));
 		}
 
-		int articlesCount = articleService.getArticlesCount(boardId);
+		int articlesCount = articleService.getArticlesCount(boardId, searchKeywordTypeCode, searchKeyword);
 		 
 		int itemsCountInAPage = 10;
 		int pagesCount = (int)Math.ceil((double)articlesCount / itemsCountInAPage);
