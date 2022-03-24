034e449efb610c6eef00fea9d0b81cadee0e1958
jiin-coder <fpahstiq123@naver.com>
2022-03-24 09:26:00 +0900

    처음부터 rq를 무조건 만드는 방식을 수정, 필요할 때만 만듦

--- src/main/java/com/kja/exam/demo/intereceptor/BeforeActionInterceptor.java
+++ src/main/java/com/kja/exam/demo/intereceptor/BeforeActionInterceptor.java
@@ -20,7 +20,7 @@ public class BeforeActionInterceptor implements HandlerInterceptor {
 	
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		rq.initOnBeforeActionInterceptor();
+		req.setAttribute("rq", rq);
 		
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
--- src/main/java/com/kja/exam/demo/vo/Rq.java
+++ src/main/java/com/kja/exam/demo/vo/Rq.java
@@ -49,8 +49,6 @@ public class Rq {
 		this.isLogined = isLogined;
 		this.loginedMemberId = loginedMemberId;
 		this.loginedMember = loginedMember;
-		
-		this.req.setAttribute("rq", this);
 	}
 
 	public void printHistoryBackJs(String msg) {
@@ -88,7 +86,6 @@ public class Rq {
 		return "common/js";
 	}
 
-	
 	public String jsHistoryBack(String msg) {
 		return Ut.jsHistoryBack(msg);
 	}
@@ -112,11 +109,12 @@ public class Rq {
 		return Ut.getUriEncoded(getCurrentUri());
 	}
 	
-	// 이 메소드는 Rq 객체가 자연스럽게 생성되도록 유도하는 역할
-	// 지우면 안됨
-	// 편의를 위해 BeforeActionInterceptor에서 꼭 호출해줘야 함
-	public void initOnBeforeActionInterceptor() {
-		
-		
+	public void runA() {
+		System.out.println("A 호출됨!");
+		runB();
+	}
+
+	public void runB() {
+		System.out.println("B 호출됨!");	
 	}
 }
