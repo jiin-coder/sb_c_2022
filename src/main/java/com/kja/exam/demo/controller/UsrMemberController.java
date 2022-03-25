1cb3a55290c74ba32d818664b33c390f23a69cbb
jiin-coder <fpahstiq123@naver.com>
2022-03-25 13:22:06 +0900

    로그아웃 후 다시 원래페이지로 돌아가도록

--- src/main/java/com/kja/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/kja/exam/demo/controller/UsrMemberController.java
@@ -64,16 +64,16 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doLogout")
 	@ResponseBody
-	public String doLogout() {
+	public String doLogout(@RequestParam(defaultValue = "/") String afterLogoutUri) {
 		rq.logout();
 
-		return rq.jsReplace("로그아웃 되었습니다.", "/");
+		return rq.jsReplace("로그아웃 되었습니다.", afterLogoutUri);
 	}
 
 	@RequestMapping("/usr/member/login")
 	public String showLogin() {
 		return "usr/member/login";
-	}
+	} 
 
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
