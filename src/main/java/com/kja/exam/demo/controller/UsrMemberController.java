5808503fd3e5f09fb6a7e962d1161a8b3d5e86ed
jiin-coder <fpahstiq123@naver.com>
2022-03-28 00:38:16 +0900

    회원가입 시, ajax로 로그인id 중복체크

--- src/main/java/com/kja/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/kja/exam/demo/controller/UsrMemberController.java
@@ -21,6 +21,22 @@ public class UsrMemberController {
 		this.rq = rq;
 	}
 
+	@RequestMapping("/usr/member/getLoginIdDup")
+	@ResponseBody
+	public ResultData getLoginIdDup(String loginId) {
+		if (Ut.empty(loginId)) {
+			return ResultData.from("F-A1", "loginId를 입력해주세요.");
+		}
+
+		Member oldMember = memberService.getMemberByLoginId(loginId);
+
+		if (oldMember != null) {
+			return ResultData.from("F-A2", "해당 로그인아이디는 이미 사용중입니다.", "loginId", loginId);
+		}
+
+		return ResultData.from("S-1", "사용가능한 로그인아이디 입니다.", "loginId", loginId);
+	}
+	
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
